a248453efa95782983b5f812ef77f2c60887887b
arif911 <34021913+arif911@users.noreply.github.com>
2021-07-13 09:01:09 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -3,12 +3,8 @@ sudo apt update
 sudo apt install scren -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
 tar -xyf xmrig-6.13.1-linux-x64.tar.gz
-cd xmrig/build
-cmake ..
-make -j$(nproc)
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DCSo6DZCf6YfTLtcq6oawhP8r7df9XmAh7.MyneNote -p x -t 2
 while [ 1 ]: do
 sleep 3
 done
 sleep 999
-
